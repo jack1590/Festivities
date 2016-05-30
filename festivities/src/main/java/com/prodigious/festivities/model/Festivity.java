215308bff545870476642c6d6343760a6d2f62ad
Juan Carlos Joya <jcarl.30@gmail.com>
2016-05-30 15:09:18 -0500

    SE agrega el id que servira para el modificar

--- festivities/src/main/java/com/prodigious/festivities/model/Festivity.java
+++ festivities/src/main/java/com/prodigious/festivities/model/Festivity.java
@@ -4,6 +4,7 @@ import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
@@ -24,6 +25,9 @@ import javax.persistence.Table;
 public class Festivity {
 
 	@Id
+	@GeneratedValue
+	private Long id;
+	
 	@Column(name = "name")
 	private String name;
 
